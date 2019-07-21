499a828a032bf36b592e3196221990fb61790651
Arun Prasaad Gunasekaran <arunprasaad2711@gmail.com>
2019-07-21 23:11:25 +0530

    Minor tweaks and moved a copy to Dropbox.

--- MakeWebsite.py
+++ MakeWebsite.py
@@ -2,43 +2,43 @@
 from jinja_support_functions import md2html_conv, file_loader, env
 from os import system as sys
 
-main_pages = ['index.html', 
-              'faq.html', 
-              'maths.html', 
+main_pages = ['index.html',
+              'faq.html',
+              'maths.html',
               'science.html',
-              'blogs.html', 
-              'apps.html', 
+              'blogs.html',
+              'apps.html',
               'syntax.html'
               ]
 titles = ['Home', 'FAQs', 'Maths', 'Science', 'Blogs', 'Apps', 'Code']
 Banners = [
-				"Hi, my name is Arun!",
-				"Hey there, Seeker!",
-				"Hey there, Maths Enthusiast!",
-				"Hey there, Science Enthusiast!",
-				"Hey there, Reader!",
-				"Hey there, Curious Mind!",
-				"Hey there, Tech Savvy!"
-			]
+	"Hi, my name is Arun!",
+	"Hey there, Seeker!",
+	"Hey there, Maths Enthusiast!",
+	"Hey there, Science Enthusiast!",
+	"Hey there, Reader!",
+	"Hey there, Curious Mind!",
+	"Hey there, Tech Savvy!"
+]
 BannerMessages = [
-				"Welcome to my website! You can read my blog posts, poems, articles, notes, from here.",
-				"You can get to know the answers to some of the common questions here!",
-				"You can refer to my mathematics subject notes here!",
-				"You can read my notes on science topics and subjects here!",
-				"You can read my blog posts, poems, short stories, and other writings here!",
-				"This is a collection of apps and recommendations I've curated! You're welcome :P",
-				"You can read my programming notes, software tips, linux notes and other writings here!"
-				]
+	"Welcome to my website! You can read my blog posts, poems, articles, notes, from here.",
+	"You can get to know the answers to some of the common questions here!",
+	"You can refer to my mathematics subject notes here!",
+	"You can read my notes on science topics and subjects here!",
+	"You can read my blog posts, poems, short stories, and other writings here!",
+	"This is a collection of apps and recommendations I've curated! You're welcome :P",
+	"You can read my programming notes, software tips, linux notes and other writings here!"
+]
 
 BannerImages = [
-				"images/banner_01.jpg",
-				"images/special_picture.jpg",
-				"images/banner_01.jpg",
-				"images/science.jpg",
-				"images/blog.png",
-				"images/banner_01.jpg",
-				"images/syntax.png",
-				]
+	"images/banner_01.jpg",
+	"images/special_picture.jpg",
+	"images/banner_01.jpg",
+	"images/science.jpg",
+	"images/blog.png",
+	"images/banner_01.jpg",
+	"images/syntax.png",
+]
 
 # Write down the list of folders, 
 # the number of characters to skip from left and right.
@@ -68,15 +68,15 @@ md2html_conv("Writings","fluidiccolours", "blog", "articles", 14, -3)
 
 i = 0
 for page in main_pages:
-
+	
 	# Templating Page
 	template = env.get_template('main_pages/'+page)
 	output = template.render(title=titles[i], Banner=Banners[i], BannerMessage=BannerMessages[i], BannerImages=BannerImages[i])
-
+	
 	# Create Page
 	with open(page, 'w') as f:
 		print(output, file=f)
-
+	
 	print("Created {} page!".format(page))
 	i += 1
 
